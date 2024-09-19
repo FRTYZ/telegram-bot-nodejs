c0f3846f23978bdc796ce58771c5d8d8b8f0434f
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-09-20 00:57:43 +0300

    Created index.js for launching bot and template engine

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('dotenv').config();
+
+// Template Engine settings
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set('views','./src/views');
+app.use("/static", express.static(path.join(__dirname, "../public")));
+
+// Helpers
+const {bot} = require('./helpers/telegramBot');  
+
+const startup = async () => {
+    
+    app.listen(PORT, () => {
+        bot.launch();
+        
+        console.log('started at ' + PORT)
+    });
+}
+startup();
